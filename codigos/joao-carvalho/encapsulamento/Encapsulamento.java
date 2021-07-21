52974d551d1bfe5a388335adc70aca593165c4f9
João Marcos Ribeiro Carvalho <joaomarcosribeiroc@gmail.com>
2021-07-21 12:31:13 -0300

    Adiciona primeiros testes com encapsulamento

--- codigos/joao-carvalho/encapsulamento/Encapsulamento.java
+++ codigos/joao-carvalho/encapsulamento/Encapsulamento.java
@@ -5,3 +5,51 @@ public class Encapsulamento{
     }
 
 }
+
+class DogFather {
+    String breed;
+    int age;
+    String color;
+ 
+    private void barking() {System.out.println("Barking");}
+    protected void sleeping() {System.out.println("Sleeping");}
+    void eat() {System.out.println("Eating");}
+    public void run() {System.out.println("Run");}
+        
+ }
+
+
+ class DogChild extends DogFather {
+     
+    private void act(){
+        // Erro, é privada da classe pai
+        super.barking();
+    }
+    
+}
+
+class ElderDog extends DogFather {
+    
+    private void act(){
+        // Protected, é acessível às classes filhas
+        super.sleeping();
+    }
+
+ }
+
+class YoungestDog extends DogFather {
+    
+    private void act(){
+        // Default, é acessível às classes filhas
+        super.sleeping();
+    }
+
+ }
+class FatestDog extends DogFather {
+    
+    private void run(){
+        // Public, é acessível às classes filhas
+        super.run();
+    }
+
+ }
